199da668c67ad1651b14c786d4290d3c4b05077d
Jabes Voltatoni <voltatoni.si@gmail.com>
2024-10-08 16:55:14 -0300

    Import Todo.js and Todo.css

---
+++ todo/src/Todo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Todo.css"
+
+function Todo(){
+    return(<div className="container"> 
+            <h1>Todo</h1> 
+            <form>
+                <input type="text"></input>
+                <button>ADD</button>
+            </form>
+            <ul>
+                <li>Item</li>
+            </ul>
+
+            </div>)
+}
+
+
+export default Todo
